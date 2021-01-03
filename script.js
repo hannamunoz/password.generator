21c55311527ed3faccb72dfdd5384a12936826a1
Hanna Munoz <hannamunoz@gmail.com>
2021-01-03 15:04:11 -0500

    adding javascript functions

--- script.js
+++ script.js
@@ -16,7 +16,48 @@ generateBtn.addEventListener("click", writePassword);
 
 // Generate random password
 function generate(){
-
 // Set password length/complexity
+let complexity = document.getElementById("slider").value;
+
+let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*+";
+
+let password = "";
+
+// Create for loop to choose password characters
+for (var i = 0; i <= complexity; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+}
+
+// Add password to textbox/display area
+document.getElementById("display").value = password;
+
+// Add password to previously generated section
+document.getElementById("lastpasswords").innerHTML += password + "<br />";
+
+}
+
+// Set default length of slider 
+document.getElementById("length").innerHTML = "Length: 25";
+
+// Function to set length depending on slider
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
+
+// Function to copy password to clipboard
+function copyPassword(){
+
+document.getElementById("display").select();
+
+document.execCommand("Copy");
+
+alert("Password copied to clipbaord");
 
 }
