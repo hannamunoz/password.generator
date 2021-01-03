84f235cac46f3e196e9928ae832ee7643c5f3ea4
Hanna Munoz <hannamunoz@gmail.com>
2021-01-03 16:54:24 -0500

    adding javascript to include checkbox options

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
 // Generate random password
-function generate(){
-// Set password length/complexity
-let complexity = document.getElementById("slider").value;
+function generate() {
+    // Set password length/complexity
+    let complexity = document.getElementById("slider").value;
 
-let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*+";
+    let uppercase = document.getElementById("uppercase").checked;
 
-let password = "";
+    let lowercase = document.getElementById("lowercase").checked;
 
-// Create for loop to choose password characters
-for (var i = 0; i <= complexity; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-}
+    let numbers = document.getElementById("numbers").checked;
 
-// Add password to textbox/display area
-document.getElementById("display").value = password;
+    let characters = document.getElementById("characters").checked;
 
-// Add password to previously generated section
-document.getElementById("lastpasswords").innerHTML += password + "<br />";
+    let uppercaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let lowercaseValues = "abcdefghijklmnopqrstuvwxyz";
+
+    let numbersValue = "1234567890"
+
+    let charactersValue = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+    let values = (uppercase ? uppercaseValues : '') + (lowercase ? lowercaseValues : '') + (numbers ? numbersValue : '') + (characters ? charactersValue : '');
+
+    let password = "";
+
+    // Check if at least one box is seected
+    if (uppercase || lowercase || numbers || characters) {
+        // Create for loop to choose password characters
+        for (var i = 0; i < complexity; i++) {
+            password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+        }
+
+        // Add password to textbox/display area
+        document.getElementById("display").value = password;
+
+        // Add password to previously generated section
+        document.getElementById("lastpasswords").innerHTML += password + "<br />";
+    } else {
+        // If no boxes are sleceted alert user to check at lease one box
+        alert("Please select at least one checkbox.")
+    }
 
 }
 
@@ -40,24 +46,17 @@ document.getElementById("lastpasswords").innerHTML += password + "<br />";
 document.getElementById("length").innerHTML = "Length: 64";
 
 // Function to set length depending on slider
-document.getElementById("slider").oninput = function(){
-
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 1";
-    }
-
+document.getElementById("slider").oninput = function () {
+    document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
 }
 
 // Function to copy password to clipboard
-function copyPassword(){
+function copyPassword() {
 
-document.getElementById("display").select();
+    document.getElementById("display").select();
 
-document.execCommand("Copy");
+    document.execCommand("Copy");
 
-alert("Password copied to clipbaord")
+    alert("Password copied to clipbaord")
 
 }
